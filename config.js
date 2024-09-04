17de01ca79ed2f32b7b7fee56f454c69bac6dc74
tharumin <94857655+tharumin@users.noreply.github.com>
2024-09-05 00:07:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,20 +3,20 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 
 //═══════[Required Variables]════════\\
-global.owner = process.env.OWNER_NUMBER.split(",")
-global.mongodb = process.env.MONGODB_URI || "Enter-MongoURI-HERE"
+global.owner = process.env.OWNER_NUMBER.split("94779062397")
+global.mongodb = process.env.MONGODB_URI || "mongodb+srv://sadeeshatharumin:alexadatabase@alexa.qo0a6.mongodb.net/?retryWrites=true&w=majority&appName=Alexa"
 global.port= process.env.PORT || 5000
 global.email = 'sam@sec.live'
 global.github = 'https://github.com/prabathLK/PRABATH-MD'
 global.location = 'Sultanpur IN'
 global.gurl = 'https://instagram.com/' // add your username
-global.devs = '94762280384';
+global.devs = '94779062397';
 global.website = 'https://github.com/prabathLK/PRABATH-MD' //wa.me/+94762280384
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://i.imgur.com/H062oEI.jpeg'
 module.exports = {
   OPENAI_API_KEY:process.env.OPENAI_API_KEY || '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',
   botname: process.env.BOT_NAME || 'PRABATH-MD',
-  dl_limit: process.env.DL_SIZE || '300',
+  dl_limit: process.env.DL_SIZE || '500',
   art: process.env.AUTO_RESTART || 'true',
   sudo: process.env.SUDO || '94762280384',
   alivepic: process.env.ALIVE_IMG || 'https://telegra.ph/file/ad25b2227fa2a1a01b707.jpg',
